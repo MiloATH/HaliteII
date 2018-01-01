0b90bc2e0d556a79bf47ede4da226f1c0edd596a
Milo Hartsoe <milo.red.duck@gmail.com>
2017-12-31 20:44:22 -0500

    Bot v18. Simple go to nearest non team entity. Try to dock at planets.

--- MyBot.cpp
+++ MyBot.cpp
@@ -14,6 +14,11 @@ struct DistanceFunc
         return p.location.get_distance_to(lhs.location) < p.location.get_distance_to(rhs.location);
     }
     
+    bool operator()(const Entity* lhs, const Entity* rhs) const
+    {
+        return p.location.get_distance_to(lhs->location) < p.location.get_distance_to(rhs->location);
+    }
+    
 private:
     Entity p;
 };
@@ -28,7 +33,7 @@ bool all_on_team(const vector<Entity> &items, PlayerId owner){
 }
 
 int main() {
-    const hlt::Metadata metadata = hlt::initialize("UpClose");
+    const hlt::Metadata metadata = hlt::initialize("PassiveDef");
     const hlt::PlayerId player_id = metadata.player_id;
 
     const hlt::Map& initial_map = metadata.initial_map;
@@ -60,13 +65,65 @@ int main() {
             }
         }
         
+        // Compute vector of all non team entities
+        vector<const Entity*> non_team_entities;
+        for(auto &kv : map.ships) {
+            if(kv.first != player_id) {
+                for(int i = 0; i < kv.second.size(); ++i) {
+                    non_team_entities.push_back(&(kv.second[i]));
+                }
+            }
+        }
+        for(int i = 0; i < map.planets.size(); ++i) {
+            non_team_entities.push_back(&(map.planets[i]));
+        }
         
         for (const hlt::Ship& ship : map.ships.at(player_id)) {
             bool hasCommand = false;
             if (ship.docking_status != hlt::ShipDockingStatus::Undocked) {
                 continue;
             }
-            std::vector<hlt::Planet> planets = map.planets;
+            sort(non_team_entities.begin(), non_team_entities.end(), DistanceFunc(ship));
+            for(const Entity* entity : non_team_entities) {
+                // Go to / dock at closest entity
+                // Check if planets and can dock
+                if(const Planet* planet = static_cast<const Planet*>(entity)) {
+                    if (planet->is_full() && planet->owned && planet->owner_id == player_id){
+                        continue;
+                    }
+                    
+                    if (ship.can_dock(*planet) && !hasCommand) {
+                        if ((!(planet->owned) || planet->owner_id == player_id)){
+                            moves.push_back(hlt::Move::dock(ship.entity_id, planet->entity_id));
+                            hasCommand = true;
+                            break;
+                        } else {
+                            // Already at the planet, but currently someone else owns the planet.
+                            // Thus, move to attacking nearest ship
+                            continue;
+                        }
+                    }
+                    
+                    const hlt::possibly<hlt::Move> move =
+                    hlt::navigation::navigate_ship_to_dock(map, ship, *planet, hlt::constants::MAX_SPEED);
+                    if (move.second && !hasCommand) {
+                        moves.push_back(move.first);
+                        hasCommand = true;
+                        break;
+                    }
+                }
+                else {
+                    // Move towards entity if possible
+                    const hlt::possibly<hlt::Move> move =
+                    hlt::navigation::navigate_ship_to_dock(map, ship, *entity, hlt::constants::MAX_SPEED);
+                    if (move.second && !hasCommand) {
+                        moves.push_back(move.first);
+                        hasCommand = true;
+                        break;
+                    }
+                }
+            }
+            /*std::vector<hlt::Planet> planets = map.planets;
             std::sort(planets.begin(), planets.end(), DistanceFunc(ship));
             for (const hlt::Planet& planet : planets) {
                 // Skip over this planet if it is owned by an opponent, or I own it and it is full
@@ -75,7 +132,7 @@ int main() {
                // if (planet.owned && (planet.owner_id != player_id || planet.is_full())) {
                     continue;
                 }
-                
+             
                 if (ship.can_dock(planet) && !hasCommand) {
                     if ((!planet.owned || planet.owner_id == player_id)){
                         moves.push_back(hlt::Move::dock(ship.entity_id, planet.entity_id));
@@ -86,7 +143,7 @@ int main() {
                         break;
                     }
                 }
-                
+             
                 const hlt::possibly<hlt::Move> move =
                         hlt::navigation::navigate_ship_to_dock(map, ship, planet, hlt::constants::MAX_SPEED);
                 if (move.second && !hasCommand) {
@@ -97,18 +154,18 @@ int main() {
             }
             if (!hasCommand){
                 // Attack nearest enemy ship
-                
+             
                 // Find ships not on my team
                 vector<Ship> enemies = docked_enemies;
-                
+             
                 // Find all enemy ships
                 /* for(const pair<PlayerId, std::vector<Ship>> players_ships: map.ships){
                     if(players_ships.first != player_id) {
                         enemies.insert(enemies.end(), players_ships.second.begin(), players_ships.second.end());
                     }
                 } */
-                
-                
+            
+                /*
                 // Sort by distance from me (ship)
                 sort(enemies.begin(), enemies.end(), DistanceFunc(ship));
                 for(Ship enemy: enemies) {
@@ -120,7 +177,7 @@ int main() {
                         break;
                     }
                 }
-            }
+            }*/
         }
 
         if (!hlt::out::send_moves(moves)) {
